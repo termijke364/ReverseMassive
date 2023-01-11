c9988c0e66194640a902c2e4e4c7268b1dbc4ab8
termijke364 <122256312+termijke364@users.noreply.github.com>
2023-01-11 15:04:35 +0400

    Add files via upload

---
+++ ReverseMassive.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int const arsize = 10;
+
+void ReverseMassive(int arr[], int arr1[])
+{
+	for (int i = 0; i < arsize; i++)
+		arr[i] = rand() % 10 + 1;
+
+	for (int i = 0; i < arsize; i++)
+		arr1[i] = arr[arsize - i-1];
+}
+int main()
+{
+	setlocale(0, "");
+	srand(time(NULL));
+
+	int arr[arsize]{};
+	int arr1[arsize]{};
+	ReverseMassive(arr, arr1);
+	
+	cout << "Заданный массив" << endl;
+
+	for (int i = 0; i < arsize; i++)
+		cout << arr[i] << ' ';
+
+	cout << endl;
+
+	cout << "Обратный массив" << endl;
+
+	for (int i = 0; i < arsize; i++)
+		cout << arr1[i] << ' ';
+
+	cout << endl;
+
+	return 0;
+}
